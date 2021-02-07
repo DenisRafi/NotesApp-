ba4210e959963bdc1d0a3aad632d030e7bcedfad
DenisRafi <62852838+DenisRafi@users.noreply.github.com>
2021-02-07 15:21:53 +0000

    Add files via upload

---
+++ .cs
@@ -0,0 +1,10 @@
+<Window x:Class="MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="Notes App • ByDR" Height="326.562" Width="433.226">
+    <StackPanel>
+        <TextBlock Margin="10" Foreground="red"><Run Text="Hello"/><Run Text=", Denis "/></TextBlock>
+        <TextBlock Margin="10" TextTrimming="CharacterEllipsis" Height="38" ><Run Foreground="Red" Text="G"/><Run Foreground="Black" Text="it"/><Run Foreground="Black" Text="H"/><Run Foreground="Blue" Text="ub"/><Run Foreground="Blue" Text=" "/><Run Foreground="Blue" Text="•"/><Run Foreground="Blue" Text=" "/><Run Foreground="Blue" Text="I"/><Run Foreground="BlueViolet" Text="nstagram"/><Run Foreground="Black" Text=" • Y"/><Run Foreground="Blue" Text="ou"/><Run Foreground="Blue" Text="T"/><Run Foreground="Red" Text="ube"/><Run Foreground="Green" Text=" • Blog"/></TextBlock>
+        <TextBlock Margin="10" TextWrapping="Wrap" Foreground="Green" Height="104"><Run Text="https://github.com/"/><Run Text="d"/><Run Text="enis"/><Run Text="r"/><Run Text="afi"/><Run Text=" ​"/><LineBreak/><Run Text="https://www.instagram.com/denisrafi1"/><LineBreak/><Run Text="http://youtube.com/denisrafi"/><Run Text=" "/><LineBreak/><Run Text="https://drafi1.blogspot.com"/><LineBreak/><Run/><LineBreak/><Run/></TextBlock>
+    </StackPanel>
+</Window>
